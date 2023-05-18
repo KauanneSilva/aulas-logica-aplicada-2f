bb84c25b957bb2ecf2f6a4172db4fda6029c1159
kauannesilva <Kauannesilv2405@gmail.com>
2023-05-18 08:26:49 -0300

    "falei" "falei"

---
+++ src/Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa {
+    public String sobrenome;
+    // atributos da classe
+    String nome;
+    String Sobrenome;
+    // Método da classe
+    public void falar(){
+        System.out.println("falei");
+
+    }
+}
